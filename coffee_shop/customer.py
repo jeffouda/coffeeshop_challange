4f83e850d1f5b828cb2a4c2b8e6dafce255fc998
Jeff Ouda <jeffoudahabakuk@gmail.com>
2025-11-24 11:37:19 +0300

    Add Customer class with properties and relationship methods

---
+++ coffee_shop/customer.py
@@ -0,0 +1,46 @@
+class Customer:
+    def __init__(self, name):
+        self.name = name
+        self._orders = []
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str):
+            raise TypeError("Customer name must be a string")
+        if not (1 <= len(value) <= 15):
+            raise ValueError("Customer name must be between 1 and 15 characters")
+        self._name = value
+
+    def orders(self):
+        return list(self._orders)
+
+    def coffees(self):
+        unique_coffees = []
+        for order in self._orders:
+            coffee = order.coffee
+            if coffee not in unique_coffees:
+                unique_coffees.append(coffee)
+        return unique_coffees
+
+    def create_order(self, coffee, price):
+        from .order import Order
+
+        order = Order(self, coffee, price)
+        self._orders.append(order)
+        coffee._add_order(order)
+        return order
+
+    @classmethod
+    def most_aficionado(cls, coffee):
+        from collections import defaultdict
+
+        spend_per_customer = defaultdict(float)
+        for order in coffee.orders():
+            spend_per_customer[order.customer] += order.price
+        if not spend_per_customer:
+            return None
+        return max(spend_per_customer, key=spend_per_customer.get)
